78abeb8a2c1ad77d52dae5b0ccab6965e7b112ee
ClecianoPedro <itzcleciano@gmail.com>
2025-07-20 18:30:22 -0300

    Retirado print de teste

--- main.py
+++ main.py
@@ -25,7 +25,4 @@ with app.app_context():
 
 # Iniciando o app
 if __name__ == '__main__':
-    print("\n📌 Rotas registradas:")
-    for rule in app.url_map.iter_rules():
-        print(f"{rule.endpoint} -> {rule.rule} [{', '.join(rule.methods)}]")
     app.run(host='0.0.0.0', port=5000, debug=True)
